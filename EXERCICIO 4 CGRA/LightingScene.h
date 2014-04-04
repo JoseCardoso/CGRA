571c3bcf5ce98cec24f70f71af9e6c5b570db6de
Jose <josecardos@gmail.com>
2014-04-04 14:29:14 +0100

    Exercicio 4: Adding Textures

---
+++ EXERCICIO 4 CGRA/LightingScene.h
@@ -0,0 +1,41 @@
+#ifndef LightingScene_H
+#define LightingScene_H
+
+#include "CGFscene.h"
+#include "CGFappearance.h"
+#include "myTable.h"
+#include "Plane.h"
+#include "myCylinder.h"
+
+class LightingScene : public CGFscene
+{
+public:
+	void init();
+	void display();
+
+	CGFlight* light0;
+	CGFlight* light1;
+	CGFlight* light2;
+	CGFlight* light3;
+
+
+	myTable* table;
+	myCylinder* cylinder;
+	myCylinder* cylinder1;
+	Plane* wall;
+	Plane* boardA;
+	Plane* boardB;
+	CGFappearance* materialA;
+	CGFappearance* materialB;
+
+	CGFappearance* floorAppearence;
+	CGFappearance* tableAppearence;
+	CGFappearance* slidesAppearence;
+	CGFappearance* wallAppearence;
+	CGFappearance* boardAppearence;
+	CGFappearance* windowAppearence;
+	
+	~LightingScene();
+};
+
+#endif
---
+++ EXERCICIO 4 CGRA/myUnitCube.cpp
@@ -0,0 +1,180 @@
+#include "myUnitCube.h"
+
+
+void myUnitCube::drawNoTexture()
+{
+	glPushMatrix();
+	glTranslated(-0.5,-0.5,-0.5);
+
+	//face de tras
+	glPushMatrix();
+	glRotated(180,0,1,0);
+	glTranslated(-1,0,0);
+	glBegin(GL_QUADS);
+		glNormal3d(0,0,1);
+		glVertex3d(0,0,0);
+		glVertex3d(1,0,0);
+		glVertex3d(1,1,0);
+		glVertex3d(0,1,0);
+	glEnd();
+	glPopMatrix();
+
+
+	//face da frente
+	glPushMatrix();
+	glTranslated(0,0,1);
+	glBegin(GL_QUADS);
+		glNormal3d(0,0,1);
+		glVertex3d(0,0,0);
+		glVertex3d(1,0,0);
+		glVertex3d(1,1,0);
+		glVertex3d(0,1,0);
+	glEnd();
+	glPopMatrix();
+
+
+	//face de baixo
+	glPushMatrix();
+	glRotated(90,1,0,0);
+	glBegin(GL_QUADS);
+		glNormal3d(0,0,1);
+		glVertex3d(0,0,0);
+		glVertex3d(1,0,0);
+		glVertex3d(1,1,0);
+		glVertex3d(0,1,0);
+	glEnd();
+	glPopMatrix();
+
+	//face de cima
+	glPushMatrix();
+	glRotated(-90,1,0,0);
+	glTranslated(0,-1,1);
+	glBegin(GL_QUADS);
+		glNormal3d(0,0,1);
+		glVertex3d(0,0,0);
+		glVertex3d(1,0,0);
+		glVertex3d(1,1,0);
+		glVertex3d(0,1,0);
+	glEnd();
+	glPopMatrix();
+
+
+
+	//face da esquerda
+	glPushMatrix();
+	glRotated(-90,0,1,0);
+	glBegin(GL_QUADS);
+		glNormal3d(0,0,1);
+		glVertex3d(0,0,0);
+		glVertex3d(1,0,0);
+		glVertex3d(1,1,0);
+		glVertex3d(0,1,0);
+	glEnd();
+	glPopMatrix();
+
+	//face da direita
+	glPushMatrix();
+	glRotated(90,0,1,0);
+	glTranslated(-1,0,1);
+	glBegin(GL_QUADS);
+		glNormal3d(0,0,1);
+		glVertex3d(0,0,0);
+		glVertex3d(1,0,0);
+		glVertex3d(1,1,0);
+		glVertex3d(0,1,0);
+	glEnd();
+	glPopMatrix();
+	glPopMatrix();
+
+
+}
+
+
+void myUnitCube::drawTextures()
+{
+	glPushMatrix();
+	glTranslated(-0.5,-0.5,-0.5);
+
+	//face de tras
+	glPushMatrix();
+	glRotated(180,0,1,0);
+	glTranslated(-1,0,0);
+	glBegin(GL_QUADS);
+		glNormal3d(0,0,1);
+		glTexCoord2d(0,0);glVertex3d(0,0,0);
+		glTexCoord2d(1,0);glVertex3d(1,0,0);
+		glTexCoord2d(1,1);glVertex3d(1,1,0);
+		glTexCoord2d(0,1);glVertex3d(0,1,0);
+	glEnd();
+	glPopMatrix();
+
+
+	//face da frente
+	glPushMatrix();
+	glTranslated(0,0,1);
+	glBegin(GL_QUADS);
+		glNormal3d(0,0,1);
+		glTexCoord2d(0,0);glVertex3d(0,0,0);
+		glTexCoord2d(1,0);glVertex3d(1,0,0);
+		glTexCoord2d(1,1);glVertex3d(1,1,0);
+		glTexCoord2d(0,1);glVertex3d(0,1,0);
+	glEnd();
+	glPopMatrix();
+
+
+	//face de baixo
+	glPushMatrix();
+	glRotated(90,1,0,0);
+	glBegin(GL_QUADS);
+		glNormal3d(0,0,1);
+		glTexCoord2d(0,0);glVertex3d(0,0,0);
+		glTexCoord2d(1,0);glVertex3d(1,0,0);
+		glTexCoord2d(1,1);glVertex3d(1,1,0);
+		glTexCoord2d(0,1);glVertex3d(0,1,0);
+	glEnd();
+	glPopMatrix();
+
+	//face de cima
+	glPushMatrix();
+	glRotated(-90,1,0,0);
+	glTranslated(0,-1,1);
+	glBegin(GL_QUADS);
+		glNormal3d(0,0,1);
+		glTexCoord2d(0,0);glVertex3d(0,0,0);
+		glTexCoord2d(1,0);glVertex3d(1,0,0);
+		glTexCoord2d(1,1);glVertex3d(1,1,0);
+		glTexCoord2d(0,1);glVertex3d(0,1,0);
+	glEnd();
+	glPopMatrix();
+
+
+
+	//face da esquerda
+	glPushMatrix();
+	glRotated(-90,0,1,0);
+	glBegin(GL_QUADS);
+		glNormal3d(0,0,1);
+		glTexCoord2d(0,0);glVertex3d(0,0,0);
+		glTexCoord2d(1,0);glVertex3d(1,0,0);
+		glTexCoord2d(1,1);glVertex3d(1,1,0);
+		glTexCoord2d(0,1);glVertex3d(0,1,0);
+	glEnd();
+	glPopMatrix();
+
+	//face da direita
+	glPushMatrix();
+	glRotated(90,0,1,0);
+	glTranslated(-1,0,1);
+	glBegin(GL_QUADS);
+		glNormal3d(0,0,1);
+		glTexCoord2d(0,0);glVertex3d(0,0,0);
+		glTexCoord2d(1,0);glVertex3d(1,0,0);
+		glTexCoord2d(1,1);glVertex3d(1,1,0);
+		glTexCoord2d(0,1);glVertex3d(0,1,0);
+	glEnd();
+	glPopMatrix();
+	glPopMatrix();
+
+
+}
+
